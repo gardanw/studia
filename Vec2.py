6f48c82fe2090bcf4b32502900648cef58edb240
Damian Sztuczka <gardanw@gmail.com>
2019-11-04 16:23:24 +0100

    dodano iloczyn, dlugosc i normalizacje do Vec2

--- Vec2.py
+++ Vec2.py
@@ -1,21 +1,24 @@
+import math
+from copy import deepcopy as dc
+
 class Vec2 :
 
-    def __init__(self, *args: float()):
+    def __init__(self, *args):
         if type(args[0]) == list:
-            self.x = args[0][0]
-            self.y = args[0][1]
+            self.__x = args[0][0]
+            self.__y = args[0][1]
         else:
-            self.x = args[0]
-            self.y = args[1]
+            self.__x = args[0]
+            self.__y = args[1]
 
     def __str__(self):
         string = str()
-        string += str(self.x) + ', ' + str(self.y)
+        string += str(self.__x) + ', ' + str(self.__y)
         return string
 
     def add(self, other):
-        self.x += other.x
-        self.y += other.y
+        self.__x += other.__x
+        self.__y += other.__y
         return self
 
     def __add__(self, other):
@@ -24,21 +27,35 @@ class Vec2 :
     def __iadd__(self, other):
         return self.add(other)
 
-    def sub(self, other):
-        self.x -= other.x
-        self.y -= other.y
+    def sub(self, other): 
+        self.__x -= other.__x
+        self.__y -= other.__y
         return self
 
-    def __sub__(self, other):
+    def __sub__(self, other): 
         return self.sub(other)
 
-    def __isub__(self, other):
+    def __isub__(self, other): 
         return self.sub(other)
-
+    
+    def dp(self, other):
+        return (self.__x * other.__x) + (self.__y * other.__y)
+    
+    def length(self):
+        return (self.__x**2 + self.__y**2)**(0.5)
+    
+    def normalize(self):
+        new = dc(self)
+        new.__x = new.__x/self.length()
+        new.__y = new.__y/self.length()
+        return new
 
 
 if __name__ == "__main__":
-    w1 = Vec2([1, 2])
-    w2 = Vec2(1, 3)
-    w2 -= w1
-    print(w2)
+    w1 = Vec2(5,1)
+    w2 = Vec2(-2,4)
+    print(w1.length(), w2.length())
+    print(w1.dp(w2))
+    print(w1.normalize().length())
+#    w1 -= w2
+#    print(w1.length())
