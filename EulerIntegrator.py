0722e1513034443f3be1c6de8ace4305b8818216
gardanw@gmail.com <dsztuczka@ws474-9.bioex.fuw.edu.pl>
2019-11-15 17:45:25 +0100

    zmodyfikowany system

---
+++ EulerIntegrator.py
@@ -0,0 +1,4 @@
+class EulerIntegrator :
+    def __init__(self, system):
+        self.__system = system
+
--- System.py
+++ System.py
@@ -1,6 +1,22 @@
+from Atom import Atom
+from Vec2 import Vec2
+
 class System :
-    def __init__(self, atoms, scene, polygons):
-        pass
+    def __init__(self, atoms=list(), scene=list()):
+        self.__atoms = atoms
+        self.__scena = scene
 
     def __str__(self):
-        pass
+        return 'liczba atomow w systemie = ' + str(len(self.__atoms)) + ' obiekty w scenie = ' + str(len(self.__scena))
+
+    @property
+    def get_atoms(self):
+        return self.__atoms
+
+    @property
+    def get_scena(self):
+        return self.__scena
+
+if __name__ == '__main__':
+    s = System([Atom(v=Vec2(1, 2), pos=Vec2(1, 1)), Atom(v=Vec2(1, 2), pos=Vec2(2, 1))])
+    print(s)
